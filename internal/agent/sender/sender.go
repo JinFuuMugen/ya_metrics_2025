8f31b47abc3cb443faa8241a6bc893a0716881e9
Alexey Kalinin <leshakalinin93@gmail.com>
2025-07-10 15:38:48 +0300

    fixed unclosed body

--- internal/agent/sender/sender.go
+++ internal/agent/sender/sender.go
@@ -21,26 +21,30 @@ func NewSender() *values {
 }
 
 func (v *values) Process(counters []storage.Counter, gauges []storage.Gauge) error {
-	var err error
 
 	for _, c := range counters {
 
 		url := fmt.Sprintf("%s/update/%s/%s/%s", v.addr, c.GetType(), c.GetName(), c.GetValueString())
 
-		_, err = v.client.Post(url, "text/plain", nil)
+		resp, err := v.client.Post(url, "text/plain", nil)
 		if err != nil {
 			return fmt.Errorf("cannot send counter metric: %w", err)
 		}
+
+		defer resp.Body.Close()
+
 	}
 
 	for _, g := range gauges {
 
 		url := fmt.Sprintf("%s/update/%s/%s/%s", v.addr, g.GetType(), g.GetName(), g.GetValueString())
 
-		_, err = v.client.Post(url, "text/plain", nil)
+		resp, err := v.client.Post(url, "text/plain", nil)
 		if err != nil {
 			return fmt.Errorf("cannot send gauge metric: %w", err)
 		}
+
+		defer resp.Body.Close()
 	}
 
 	return nil

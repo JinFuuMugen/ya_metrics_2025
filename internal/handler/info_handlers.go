2925c3d1d134bd86c1aa37a1e5d7ce26cc8c7490
Alexey Kalinin <leshakalinin93@gmail.com>
2025-07-31 12:28:00 +0300

    added zap logger

--- internal/handler/info_handlers.go
+++ internal/handler/info_handlers.go
@@ -2,9 +2,9 @@ package handler
 
 import (
 	"html/template"
-	"log"
 	"net/http"
 
+	"github.com/JinFuuMugen/ya_metrics_2025/internal/logger"
 	"github.com/JinFuuMugen/ya_metrics_2025/internal/storage"
 )
 
@@ -58,7 +58,7 @@ func InfoPageHandler(w http.ResponseWriter, r *http.Request) {
 
 	data := PageData{Counters: counters, Gauges: gauges}
 	if err := tmpl.Execute(w, data); err != nil {
-		log.Printf("unable to parse info page: %s", err)
+		logger.Errorf("unable to parse info page: %w", err)
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
